ce57285f61bcfaf3a1db2cbe4b1494ad5b1d26b0
trycatchlearn <trycatchlearn@outlook.com>
2019-11-14 15:44:37 +0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -48,6 +55,8 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
