98706587298e588641839ca00f37c4d3a63d56cd
trycatchlearn <trycatchlearn@outlook.com>
2019-11-16 13:09:26 +0700

    End of section 21

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,53 +1,57 @@
+export interface IActivitiesEnvelope {
+  activities: IActivity[];
+  activityCount: number;
+}
+
 export interface IActivity {
-    id: string;
-    title: string;
-    description: string;
-    category: string;
-    date: Date;
-    city: string;
-    venue: string;
-    isGoing: boolean;
-    isHost: boolean;
-    attendees: IAttendee[];
-    comments: IComment[];
-  }
-  
-  export interface IComment {
-    id: string;
-    createdAt: Date;
-    body: string;
-    username: string;
-    displayName: string;
-    image: string;
-  }
-  
-  export interface IActivityFormValues extends Partial<IActivity> {
-    time?: Date;
-  }
-  
-  export class ActivityFormValues implements IActivityFormValues {
-    id?: string = undefined;
-    title: string = '';
-    category: string = '';
-    description: string = '';
-    date?: Date = undefined;
-    time?: Date = undefined;
-    city: string = '';
-    venue: string = '';
-  
-    constructor(init?: IActivityFormValues) {
-      if (init && init.date) {
-        init.time = init.date;
-      }
-      Object.assign(this, init);
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  date: Date;
+  city: string;
+  venue: string;
+  isGoing: boolean;
+  isHost: boolean;
+  attendees: IAttendee[];
+  comments: IComment[];
+}
+
+export interface IComment {
+  id: string;
+  createdAt: Date;
+  body: string;
+  username: string;
+  displayName: string;
+  image: string;
+}
+
+export interface IActivityFormValues extends Partial<IActivity> {
+  time?: Date;
+}
+
+export class ActivityFormValues implements IActivityFormValues {
+  id?: string = undefined;
+  title: string = '';
+  category: string = '';
+  description: string = '';
+  date?: Date = undefined;
+  time?: Date = undefined;
+  city: string = '';
+  venue: string = '';
+
+  constructor(init?: IActivityFormValues) {
+    if (init && init.date) {
+      init.time = init.date;
     }
+    Object.assign(this, init);
   }
-  
-  export interface IAttendee {
-    username: string;
-    displayName: string;
-    image: string;
-    isHost: boolean;
-    following?: boolean;
-  }
-  
+}
+
+export interface IAttendee {
+  username: string;
+  displayName: string;
+  image: string;
+  isHost: boolean;
+  following?: boolean;
+}
--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -1,17 +1,23 @@
 export interface IProfile {
-    displayName: string;
-    username: string;
-    bio: string;
-    image: string;
-    following: boolean;
-    followersCount: number;
-    followingCount: number;
-    photos: IPhoto[];
-  }
-  
-  export interface IPhoto {
-    id: string;
-    url: string;
-    isMain: boolean;
-  }
-  
+  displayName: string;
+  username: string;
+  bio: string;
+  image: string;
+  following: boolean;
+  followersCount: number;
+  followingCount: number;
+  photos: IPhoto[];
+}
+
+export interface IPhoto {
+  id: string;
+  url: string;
+  isMain: boolean;
+}
+
+export interface IUserActivity {
+  id: string;
+  title: string;
+  category: string;
+  date: Date;
+}
